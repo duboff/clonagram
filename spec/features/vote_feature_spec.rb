e3532a3b36ff44e17e123a66090e07f42a272eea
Mikhail Dubov <mdubov@gmail.com>
2014-04-23 17:22:29 +0100

    Added tests for emails

--- spec/features/vote_feature_spec.rb
+++ spec/features/vote_feature_spec.rb
@@ -8,7 +8,7 @@ describe 'Voting on a post' do
   describe 'upvoting' do
     before do
       clear_emails
-      # login_as create(:user2)
+      login_as create(:user2)
     end
 
     it 'score increases when post is upvoted', js: true do
@@ -19,16 +19,23 @@ describe 'Voting on a post' do
     it 'sends email when a post is upvoted', js: true do
       visit '/posts'
       page.find('.upvote').click
+      open_emai('user1@example.com')
+      expect(current_email).to have_content 'Your photo just got upvoted'
     end
   end
+  describe 'upvoting' do
 
-
-
-  it 'downvotes a post', js: true do
-    visit '/posts'
-    page.find('.downvote').click
-    # click_link icon('thumbs-o-down')
-    expect(page).to have_css '.vote_count', text: '-1'
+    it 'downvotes a post', js: true do
+      visit '/posts'
+      page.find('.downvote').click
+      expect(page).to have_css '.vote_count', text: '-1'
+    end
+    it 'sends email when a post is upvoted', js: true do
+      visit '/posts'
+      page.find('.downvote').click
+      open_emai('user1@example.com')
+      expect(current_email).to have_content 'Your photo just got downvoted'
+    end
   end
 
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,9 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+  config.include Warden::Test::Helpers
+  Warden.test_mode!
+
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
